5121c55727994e1427686a8e57ef2b3a66e5b382
SimranRaina <simran.pre22@gmail.com>
2020-05-24 14:16:35 +0530

    Updated index.html

--- app.py
+++ app.py
@@ -23,7 +23,7 @@ def predict():
 
     output = round(prediction[0], 2)
 
-    return render_template('index.html', prediction_text='Estimated Salary {}'.format(output))
+    return render_template('index.html', prediction_text='Estimated Salary (USD): ${}'.format(output))
 
 if __name__ == "__main__":
     app.run(debug=True)
